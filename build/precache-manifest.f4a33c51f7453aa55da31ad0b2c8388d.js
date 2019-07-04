9e749f5c00898be1c1fcac51946e649326baded4
kunal kakade <kunal.kakade@rakuten.com>
2019-07-04 13:27:37 +0900

    inital commit

---
+++ build/precache-manifest.f4a33c51f7453aa55da31ad0b2c8388d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377f1129139f066826214c1a279d09fd",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "d5e1d2060338a9ad4ada",
+    "url": "/myapp/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/myapp/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "d5e1d2060338a9ad4ada",
+    "url": "/myapp/static/js/main.0ccbbfbf.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  }
+]);
